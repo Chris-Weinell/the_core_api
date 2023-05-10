139d6821aa0dba60b156f1412c88c3fbb88fb5c7
Chris Weinell <weinellcj@live.com>
2023-05-10 01:11:47 -0400

    update syntax

--- app/location/urls.py
+++ app/location/urls.py
@@ -13,10 +13,10 @@ from location import views
 router = DefaultRouter()
 router.register('caverns', views.CavernsViewSet)
 router.register('links', views.LinksViewSet)
-router.register('favicon', views.FaviconView)
 
 app_name = 'location'
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('favicon.ico', views.FaviconView.as_view()),
 ]
